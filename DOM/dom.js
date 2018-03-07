4e4ccbe7ee09e20905dabd6b20dd27044050d0fa
Mr <p176053@nu.edu.pk>
2018-03-07 10:59:58 +0500

    JS!

---
+++ DOM/dom.js
@@ -0,0 +1,18 @@
+// Examine the DOM
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.all);
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.links);
+// console.log(document.all[10]);
+// console.log(document.forms[0]);
+// document.title = "Hello";    changing directly the title
+// console.log(document.nodeName);
+
+
+// SELECTORS //
+
+// console.log(document.getElementById('header-title'));
