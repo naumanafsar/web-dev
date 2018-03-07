caffeeac146715641be5e9108f9f7d86f8fed9f6
Mr <p176053@nu.edu.pk>
2018-03-07 16:18:51 +0500

    DOM Part1

--- DOM/dom.js
+++ DOM/dom.js
@@ -13,6 +13,17 @@
 // console.log(document.nodeName);
 
 
-// SELECTORS //
+// GETELEMENTBYID //
 
 // console.log(document.getElementById('header-title'));
+  // var Title = document.getElementById('header-title');
+  // console.log(Title);
+  // var Title = document.getElementById('main-header');
+  // Title.textContent = 'Hello World!';  //It Ignores Style
+  // Title.innerText = "Hello From Text!";
+  // Title.style.borderBottom = "solid 3px #000";
+
+// GETELEMENTBYCLASS //
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// items[0].textContent = "Hello";
