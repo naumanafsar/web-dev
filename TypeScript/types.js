921ee54ee8377afebe3424047da05a731e5729d0
Mr <p176053@nu.edu.pk>
2018-03-21 13:09:36 +0500

    TS

---
+++ TypeScript/types.js
@@ -0,0 +1,7 @@
+var myString;
+var myNum;
+var myArr;
+myString = "Hello World";
+myNum = 23;
+console.log(myString);
+console.log(myNum);
---
+++ TypeScript/types.ts
@@ -0,0 +1,7 @@
+let myString: string;
+let myNum: number;
+// let myArr: ;
+myString = "Hello World";
+myNum = 23;
+console.log(myString);
+console.log(myNum);
