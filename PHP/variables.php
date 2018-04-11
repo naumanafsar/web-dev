282e121f0fa1d63401f509b464b1f3d80a38e11a
Mr <p176053@nu.edu.pk>
2018-04-11 11:27:52 +0500

    PHP Variables!

---
+++ PHP/variables.php
@@ -0,0 +1,46 @@
+<?php
+  //Single line comment
+
+  # Single Line Comment
+
+  /* Multi line comment
+    This is a multi line comment */
+
+  # Variables
+    /*
+      - Prefix $
+      - Starts with a letter or an underscore
+      - Only letters, numbers or underscores
+      -Case sensitive
+    */
+
+   # Data Types
+      /*
+        - Strings
+        - integers
+        - Floats
+        - Boolean
+        - Array
+        - NULL
+        - Resource
+      */
+
+    $string = "Hello World!";
+    $int1 = 4;
+    $int2 = 12;
+    $sum = $int1 + $int2;
+    // echo $sum;
+
+    $str1 = "Hello";
+    $str2 = "World";
+    echo $conc = $str1 ." ". $str2."!";
+    $float1 = 4.4;
+    $bool1 = true;
+
+    // Constants
+
+    define('GREETING', 'Hello Everyone',true);  // true to make it case insensitive
+    echo greeting;
+
+
+?>
